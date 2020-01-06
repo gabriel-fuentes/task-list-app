945a819f8b1197aad7729ed4f0b97a9c03eae64b
Gabriel Fuentes <gabrielfuentes.dev@gmail.com>
2020-01-06 18:20:33 -0400

    added a filter function

--- app.js
+++ app.js
@@ -2,11 +2,14 @@
 const form = document.querySelector('#task-form');
 const taskInput = document.querySelector('#task');
 const taskList = document.querySelector('.collection');
+const filter = document.querySelector('#filter');
 
 //Event Listeners
 form.addEventListener('submit', addTask);
 taskList.addEventListener('click', deleteTask);
+filter.addEventListener('keyup', filterTask);
 
+//Functions
 function addTask (e) {    
     e.preventDefault();
     const li = document.createElement('li');
@@ -17,11 +20,24 @@ function addTask (e) {
     link.innerHTML = "<i class='fa fa-remove'></i>";
     li.appendChild(link);
     taskList.appendChild(li);
+    taskInput.value = '';
 }
 
 function deleteTask(e){
     e.preventDefault();
     if(e.target.parentElement.classList.contains('delete-item')){
         e.target.parentElement.parentElement.remove()
+    }
+}
+
+function filterTask(e){
+    let filterInput = e.target.value;
+    let listItems = document.querySelectorAll('.collection-item');
+    for(let i = 0; i < listItems.length; i++){
+        if(listItems[i].textContent.indexOf(filterInput) != -1){
+            listItems[i].style.display = 'block';
+        }else{
+            listItems[i].style.display = 'none';
+        }
     }
 }
